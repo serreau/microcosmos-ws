aac1d86fb7dd2dcdf7e490606985a7c0fbb6b83e
Anthony MS. Martin Serreau <amartin@tmaenedis.aix>
2019-09-12 17:39:59 +0200

    add some jobs webservice

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
+var bodyParser = bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
 
 var db = null
 MongoClient.connect('mongodb://root00:root00@ds215338.mlab.com:15338/heroku_1rb0rch9', function(err,database) {
     db = database.db('heroku_1rb0rch9');
 });
 
+/******JOB******/
+
 app.get('/', function(req, res) {
-	console.log("/");
     res.setHeader('Content-Type', 'text/plain');
     res.send('Vous êtes à l\'accueil');
 });
 
-app.get('/get', (req, res) => {
-	console.log("GET");
+app.post('/job/insert', (req, res) => {
+	db.collection('job').insert(req.body);
+	res.send();
+});
+
+app.delete('/job/delete/:id', (req, res) => {
+	db.collection('job').deleteOne({id : req.params.id});
+	res.send();
+});
+
+app.put('/job/update/:id', (req, res) => {
+	db.collection('job').update({id : req.params.id}, req.body);
+	res.send();
+});
+
+app.get('/job/get/:id', (req, res) => {
+    db.collection('job').find({ id: req.params.id }).toArray(function(err, results) {
+		res.send(results);
+	});
+});
+
+app.get('/job/get', (req, res) => {
     db.collection('job').find().toArray(function(err, results) {
+		res.send(results);
+	});
+});
+
+app.get('/job/get/state/:state/search/:search', (req, res) => {
+    db.collection('job').find({ state : req.params.state, name : req.params.search }).toArray(function(err, results) {
 		res.send(results);
 	});
 });
