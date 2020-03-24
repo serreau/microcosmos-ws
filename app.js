d24494b60fced340084085e7d2b72e2fc0d1f36b
serreau <7533682+serreau@users.noreply.github.com>
2020-03-24 18:28:12 +0100

    Change date format

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', function(req, res) {
 
 app.post('/job/insert', (req, res) => {
 	req.body.state = 'TODO'
-	req.body.date = Date().toString()
+	req.body.date = Date().toUTCString()
 	db.collection('job').insertOne(req.body);
 	res.send({"success" : true});
 });
